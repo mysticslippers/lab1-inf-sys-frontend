eab91a5346b250864cec0a26b4a3392e8fe1dde3
misticslippers <eternal.martyr13579@gmail.com>
2025-11-11 10:50:33 +0300

    rebuild project

--- lab1-inf-sys-frontend/src/main.tsx
+++ lab1-inf-sys-frontend/src/main.tsx
@@ -1,6 +1,18 @@
-import { createRoot } from "react-dom/client";
-import App from "./App";
-import "./styles/index.scss";
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { store } from './store';
+import App from './App';
+import './index.css';
+import './styles/global.scss';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
-const root = createRoot(document.getElementById("root")!);
-root.render(<App />);
+createRoot(document.getElementById('root')!).render(
+    <React.StrictMode>
+        <Provider store={store}>
+            <App />
+            <ToastContainer position="bottom-right" autoClose={3000} theme="colored" />
+        </Provider>
+    </React.StrictMode>
+);
