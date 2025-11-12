3a7148311e06c208d641f250c25f6219fd13da72
misticslippers <eternal.martyr13579@gmail.com>
2025-11-12 16:54:24 +0300

    fix main.tsx

--- lab1-inf-sys-frontend/src/main.tsx
+++ lab1-inf-sys-frontend/src/main.tsx
@@ -1,18 +1,14 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import { Provider } from 'react-redux';
-import { store } from './store';
-import App from './App';
-import './index.css';
-import './styles/global.scss';
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import { store } from "./app/store";
+import App from "./App";
+import "styles/globals.scss";
 
-createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
         <Provider store={store}>
             <App />
-            <ToastContainer position="bottom-right" autoClose={3000} theme="colored" />
         </Provider>
     </React.StrictMode>
 );
