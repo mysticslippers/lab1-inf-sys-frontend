7142048a446242e42aecb4c23102fcb143e8c69e
misticslippers <eternal.martyr13579@gmail.com>
2025-11-16 13:20:37 +0300

    fix vite.config.ts

--- lab1-inf-sys-frontend/vite.config.ts
+++ lab1-inf-sys-frontend/vite.config.ts
@@ -1,9 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-// @ts-ignore
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  define: {
+    global: 'window',
+  },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8080',
+        changeOrigin: true,
+      },
+      '/ws': {
+        target: 'http://localhost:8080',
+        ws: true,
+        changeOrigin: true,
+      },
+    },
+  },
+});
