f62e0b62445cb88a2a6fae164fc83d978cd61b63
misticslippers <eternal.martyr13579@gmail.com>
2025-11-17 11:11:40 +0300

    fix tailwind.config.js

--- lab1-inf-sys-frontend/tailwind.config.js
+++ lab1-inf-sys-frontend/tailwind.config.js
@@ -1,5 +1,10 @@
+// tailwind.config.js
+/** @type {import('tailwindcss').Config} */
 export default {
-    content: ['./index.html', './src/**/*.{ts,tsx,js,jsx}'],
+    content: [
+        './index.html',
+        './src/**/*.{js,ts,jsx,tsx}',
+    ],
     theme: {
         extend: {},
     },
